1f91d1e44ba3d66f2be68e0ea5e5d870e79ca10c
Илья Ериков <demdel2100@mail.ru>
2024-08-05 23:36:35 +0300

    /

--- dotNETLearning/Program.cs
+++ dotNETLearning/Program.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -73,84 +74,121 @@ app.MapRazorPages();
 
 
 
-app.Run(async(context) =>
+//app.Run(async(context) =>
+//{
+//    var response = context.Response;
+//    var request = context.Request;
+
+//    if (request.Path == "/api/user")
+//    {
+//        var message = "Incorrect data";
+//        if (request.HasJsonContentType())
+//        {
+//            var jsonoption = new JsonSerializerOptions();
+
+//            jsonoption.Converters.Add(new PersonConverter());
+
+//            var person = await request.ReadFromJsonAsync<Person>(jsonoption);
+//            if (person != null) message = $"Name: {person.Name} Age: {person.Age}";
+//        }
+
+//        await response.WriteAsJsonAsync(new { text = message });
+//    }
+//    else
+//    {
+//        response.ContentType = "text/html; charset=utf-8";
+//        await response.SendFileAsync("Pages/index.cshtml");
+//    }
+//});
+
+
+
+
+
+
+//public record Person(string Name, int Age);
+//public class PersonConverter : JsonConverter<Person>
+//{
+//    public override Person Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+//    {
+//        var personName = "Undefined";
+//        var personAge = 0;
+//        while (reader.Read())
+//        {
+//            if (reader.TokenType == JsonTokenType.PropertyName)
+//            {
+//                var propertyName = reader.GetString();
+//                reader.Read();
+//                switch (propertyName?.ToLower())
+//                {
+//                    // если свойство age и оно содержит число
+//                    case "age" when reader.TokenType == JsonTokenType.Number:
+//                        personAge = reader.GetInt32();  // считываем число из json
+//                        break;
+//                    // если свойство age и оно содержит строку
+//                    case "age" when reader.TokenType == JsonTokenType.String:
+//                        string? stringValue = reader.GetString();
+//                        // пытаемся конвертировать строку в число
+//                        if (int.TryParse(stringValue, out int value))
+//                        {
+//                            personAge = value;
+//                        }
+//                        break;
+//                    case "name":    // если свойство Name/name
+//                        string? name = reader.GetString();
+//                        if (name != null)
+//                            personName = name;
+//                        break;
+//                }
+//            }
+//        }
+//        return new Person(personName, personAge);
+//    }
+//    // сериализуем объект Person в json
+//    public override void Write(Utf8JsonWriter writer, Person person, JsonSerializerOptions options)
+//    {
+//        writer.WriteStartObject();
+//        writer.WriteString("name", person.Name);
+//        writer.WriteNumber("age", person.Age);
+
+//        writer.WriteEndObject();
+//    }
+//}
+
+List<Person> persons = new List<Person>()
+{
+    new() { Id = Guid.NewGuid().ToString(), Name = "Tom", Age = 42 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Bob", Age = 24 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Sam", Age = 35 }
+};
+
+
+app.Run(async (context) =>
 {
     var response = context.Response;
     var request = context.Request;
+    var path = request.Path;
 
-    if (request.Path == "/api/user")
+    string expressionForGuid = @"^/api/users/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$";
+    if (path == "/api/users" && request.Method == "GET")
     {
-        var message = "Incorrect data";
-        if (request.HasJsonContentType())
-        {
-            var jsonoption = new JsonSerializerOptions();
-
-            jsonoption.Converters.Add(new PersonConverter());
-
-            var person = await request.ReadFromJsonAsync<Person>(jsonoption);
-            if (person != null) message = $"Name: {person.Name} Age: {person.Age}";
-        }
-       
-        await response.WriteAsJsonAsync(new { text = message });
+        await GetAllPeople(response);
     }
-    else
+    else if (Regex.IsMatch(path, expressionForGuid) && request.Method == "GET")
     {
-        response.ContentType = "text/html; charset=utf-8";
-        await response.SendFileAsync("Pages/index.cshtml");
+        // получаем id из адреса url
+        string? id = path.Value?.Split("/")[3];
+        await GetPerson(id, response);
     }
 });
-
 
 
 app.Run();
 
 
-public record Person(string Name, int Age);
-public class PersonConverter : JsonConverter<Person>
+public class Person
 {
-    public override Person Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        var personName = "Undefined";
-        var personAge = 0;
-        while (reader.Read())
-        {
-            if (reader.TokenType == JsonTokenType.PropertyName)
-            {
-                var propertyName = reader.GetString();
-                reader.Read();
-                switch (propertyName?.ToLower())
-                {
-                    // если свойство age и оно содержит число
-                    case "age" when reader.TokenType == JsonTokenType.Number:
-                        personAge = reader.GetInt32();  // считываем число из json
-                        break;
-                    // если свойство age и оно содержит строку
-                    case "age" when reader.TokenType == JsonTokenType.String:
-                        string? stringValue = reader.GetString();
-                        // пытаемся конвертировать строку в число
-                        if (int.TryParse(stringValue, out int value))
-                        {
-                            personAge = value;
-                        }
-                        break;
-                    case "name":    // если свойство Name/name
-                        string? name = reader.GetString();
-                        if (name != null)
-                            personName = name;
-                        break;
-                }
-            }
-        }
-        return new Person(personName, personAge);
-    }
-    // сериализуем объект Person в json
-    public override void Write(Utf8JsonWriter writer, Person person, JsonSerializerOptions options)
-    {
-        writer.WriteStartObject();
-        writer.WriteString("name", person.Name);
-        writer.WriteNumber("age", person.Age);
-
-        writer.WriteEndObject();
-    }
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
 }
-
