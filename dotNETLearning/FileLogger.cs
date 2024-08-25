28a51c0fe294f9f7e6d257f415ae43e1f9e6f383
Илья Ериков <demdel2100@mail.ru>
2024-08-25 12:40:57 +0300

    Items

---
+++ dotNETLearning/FileLogger.cs
@@ -0,0 +1,53 @@
+﻿namespace dotNETLearning
+{
+    public class FileLogger(string path) : ILogger, IDisposable
+    {
+        public string FilePath = path;
+        private static object _lock = new object();
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            lock (_lock)
+            {
+                File.AppendAllText(FilePath, formatter(state, exception) + Environment.NewLine);
+            }
+        }
+    }
+
+    public class FileLoggerProvider(string path) : ILoggerProvider
+    {
+        string path = path;
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(path);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    public static class FileLoggerExtensions
+    {
+        public static ILoggingBuilder AddFile(this ILoggingBuilder builder, string filePath)
+        {
+            builder.AddProvider(new FileLoggerProvider(filePath));
+            return builder;
+        }
+    }
+}
--- dotNETLearning/Program.cs
+++ dotNETLearning/Program.cs
@@ -3,8 +3,11 @@ using System.Reflection.Metadata.Ecma335;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
+using dotNETLearning;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Options;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,61 +35,31 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
-app.Run(async (context) =>
+app.Use(async (context, next) =>
 {
     app.Logger.LogInformation($"Path: {context.Request.Path} and Time: {DateTime.Now.Date}");
-    await context.Response.WriteAsync("Hello");
+    await next.Invoke(context);
+});
+
+app.Use(async (context, next) =>
+{
+    context.Items.Add("message", "Hello world");
+    await next.Invoke(context);
+
+});
+
+app.Run(async (context) =>
+{
+    if(context.Items.ContainsKey("messages"))
+    {
+        await context.Response.WriteAsync("Hello is real"); 
+        app.Logger.LogInformation($"Message: {context.Items["message"]}");
+    }
+    else
+    {
+        await context.Response.WriteAsync("no hello :(");
+    }
 });
 
 app.Run();
 
-public class FileLogger(string path) : ILogger, IDisposable
-{
-    public string FilePath = path;
-    private static object _lock = new object();
-
-    public IDisposable BeginScope<TState>(TState state)
-    {
-        return this;
-    }
-
-    public void Dispose()
-    {
-    }
-
-    public bool IsEnabled(LogLevel logLevel)
-    {
-        return true;
-    }
-
-    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
-    {
-        lock (_lock)
-        {
-            File.AppendAllText(FilePath, formatter(state, exception) + Environment.NewLine);
-        }
-    }
-}
-
-public class FileLoggerProvider(string path) : ILoggerProvider
-{
-    string path = path;
-
-    public ILogger CreateLogger(string categoryName)
-    {
-        return new FileLogger(path);
-    }
-
-    public void Dispose()
-    {
-    }
-}
-
-public static class FileLoggerExtensions
-{
-    public static ILoggingBuilder AddFile(this ILoggingBuilder builder, string filePath)
-    {
-        builder.AddProvider(new FileLoggerProvider(filePath));
-        return builder;
-    }
-}
