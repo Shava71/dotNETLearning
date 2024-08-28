b48521d419c50d166013f981b17be8c132b3f9d8
Илья Ериков <demdel2100@mail.ru>
2024-08-28 18:59:24 +0300

    MapGet/Post/Put/Delete

--- dotNETLearning/User.cs
+++ dotNETLearning/User.cs
@@ -1,8 +1,8 @@
 ﻿namespace dotNETLearning
 {
-    public class Person
+    public class User
     {
-        public string Id { get; set; } = "";
+        public int Id { get; set; }
         public string Name { get; set; } = "";
         public int Age { get; set; }
 
