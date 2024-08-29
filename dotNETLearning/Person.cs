ec6e34fe2e2b59ca3b1441b8acca62ada1afea26
Илья Ериков <demdel2100@mail.ru>
2024-08-29 13:04:43 +0300

    JWT

---
+++ dotNETLearning/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace dotNETLearning
+{
+    public class Person(string email, string password)
+    {
+        public string email { get; set; } = email;
+        public string password { get; set; } = password;
+    }
+}
--- dotNETLearning/Program.cs
+++ dotNETLearning/Program.cs
@@ -4,26 +4,59 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 using dotNETLearning;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
+var people = new List<Person>
+{
+    new Person("timoshka@mail.ru", "12345"),
+    new Person("tima@mail.ru", "123")
+};
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication("Bearer").AddJwtBearer();
 builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        // указывает, будет ли валидироваться издатель при валидации токена
+        ValidateIssuer = true,
+        // строка, представляющая издателя
+        ValidIssuer = AuthOptions.ISSUER,
+        // будет ли валидироваться потребитель токена
+        ValidateAudience = true,
+        // установка потребителя токена
+        ValidAudience = AuthOptions.AUDIENCE,
+        // будет ли валидироваться время существования
+        ValidateLifetime = true,
+        // установка ключа безопасности
+        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+        // валидация ключа безопасности
+        ValidateIssuerSigningKey = true,
+    };
+});
+
 
 builder.Logging.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "logger.txt"));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-var app = builder.Build();
+var app = builder.Build();  
 
-
+//app.UseDefaultFiles();
+//app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -49,8 +82,42 @@ app.Use(async (context, next) =>
     await next.Invoke(context);
 });
 
-app.Map("/hello", [Authorize]() => "Hello");
+app.MapPost("/login", (Person LoginData) =>
+{
+    Person? person = people.FirstOrDefault(p => p.email == LoginData.email && p.password == LoginData.password);
+    if (person is null) return Results.Unauthorized();
+
+    var claims = new List<Claim> { new Claim(ClaimTypes.Name, person.email) };
+
+    var jwt = new JwtSecurityToken(
+        issuer: AuthOptions.ISSUER,
+        audience: AuthOptions.AUDIENCE,
+        claims: claims,
+        expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+        signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256)
+        );
+    var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+    var response = new
+    {
+        access_token = encodedJwt,
+        username = person.email
+    };
+
+    return Results.Json(response);
+});
+
+app.Map("/data", [Authorize]() => new {Message = "Hello World"});
 
 
 app.Run();
 
+
+public class AuthOptions
+{
+    public const string ISSUER = "MyAuthServer"; // издатель токена
+    public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+    const string KEY = "mysupersecret_secretsecretsecretkey!123";   // ключ для шифрации
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+}
