2d17ff4bf83394286ad1ed2dd38e763f37f7c6bc
Shava71 <demdel2100@mail.ru>
2024-10-02 22:51:37 +0300

    CORS

--- dotNETLearning/Program.cs
+++ dotNETLearning/Program.cs
@@ -1,27 +1,20 @@
 ﻿using dotNETLearning;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 
-var people = new List<Person>
-{
-    new Person("timoshka@mail.ru", "12345"),
-    new Person("tima@mail.ru", "123")
-};
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddAuthorization();
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => options.LoginPath = "/login");
-
 
 builder.Logging.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "logger.txt"));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyOrigin());
 //app.UseDefaultFiles();
 //app.UseStaticFiles();
 
@@ -49,73 +42,9 @@ app.Use(async (context, next) =>
     await next.Invoke(context);
 });
 
-app.MapGet("/login/{username}/{phoneNumber}/{company}", async (string username, string phoneNumber, string company, HttpContext context) =>
-{
-    var claims = new List<Claim> {
-        new (ClaimTypes.Name, username),
-        new (ClaimTypes.MobilePhone, phoneNumber),
-        new ("company", company),
-        new ("language", "Russian"),
-        new ("language", "English"),
-        new ("language", "Spanish"),
-
-    };
-    var claimIdentity = new ClaimsIdentity(claims, "Cookies");
-    var claimPrincipal = new ClaimsPrincipal(claimIdentity);
-
-    await context.SignInAsync(claimPrincipal);
-    return Results.Redirect("/data");
-});
-
-app.MapGet("/addage/{age}", async (HttpContext context, string age) =>
-{
-    if (context.User.Identity is ClaimsIdentity claimsIdentity)
-    {
-        claimsIdentity.AddClaims(new[] { new Claim("age", age) });
-        
-        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-        context.SignInAsync(claimsPrincipal);
-    }
-    return Results.Redirect("/data");
-});
-
-app.MapGet("/removephone", async (HttpContext context) =>
-{
-    if(context.User.Identity is ClaimsIdentity claimsIdentity)
-    {
-        var phoneNumber = claimsIdentity.FindFirst(ClaimTypes.MobilePhone);
-        if (claimsIdentity.TryRemoveClaim(phoneNumber))
-        {
-            var claimPrincipal = new ClaimsPrincipal (claimsIdentity);
-            await context.SignInAsync(claimPrincipal);
-        }
-    }
-    return Results.Redirect("/data");
-});
 
 
-app.Map("/data", (HttpContext context) =>
-{
-    var user = context.User.Identity;
-    var userName = context.User.FindFirst(ClaimTypes.Name);
-    var phoneNumber = context.User.FindFirst(ClaimTypes.MobilePhone);
-    var company = context.User.FindFirst("company");
-    var age = context.User.FindFirst("age");
-    var languages = context.User.FindAll("language");
-    var stringLanguage = "";
-    foreach (var language in languages)
-    {
-        stringLanguage = $"{stringLanguage} {language.Value}";
-    }
-    if (user.IsAuthenticated)
-        return $"Username: {userName?.Value} Phone: {phoneNumber?.Value} Company: {company?.Value} Age: {age} \nLanguages: {stringLanguage}";
-    else return "������������ �� �����������������";
-});
 
-app.MapGet("/logout", async (HttpContext context) =>
-{
-    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-    return "������ �������";
-});
 
 app.Run();
+
